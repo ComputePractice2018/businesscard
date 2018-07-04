74de03a1099af80ab3ba7b6045fc9db1a7656694
Ilay <discherge@gmail.ru>
2018-07-04 20:33:25 +0300

    Добавлена функция Задча #8

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+//GetHelloWorldString возвращает строку с приветствием
+func GetHelloWorldString(name string) string {
+	return fmt.Sprintf("Hello, %s", name)
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println(GetHelloWorldString("Ilay"))
 }
