99344a20c6aa4a559d4673a9ae00c8b2c68e7ddb
Ilay <discherge@gmail.ru>
2018-07-10 01:00:53 +0300

    Протестировать и отрефакторить сервер Задача #28

--- backend/main.go
+++ backend/main.go
@@ -6,18 +6,13 @@ import (
 
 	"github.com/ComputePractice2018/businesscard/backend/data"
 	"github.com/ComputePractice2018/businesscard/backend/server"
-	"github.com/gorilla/mux"
 )
 
 func main() {
 	//name := flag.String("name", "Ilay", "имя для приветствия")
 	//flag.Parse()
 	vcardList := data.NewVcardList()
-	router := mux.NewRouter()
-	router.HandleFunc("/api/businesscard/vcards", server.GetVcards(vcardList)).Methods("GET")
-	router.HandleFunc("/api/businesscard/vcards", server.AddVcard(vcardList)).Methods("POST")
-	router.HandleFunc("/api/businesscard/vcards/{id}", server.EditVcard(vcardList)).Methods("PUT")
-	router.HandleFunc("/api/businesscard/vcards/{id}", server.DeleteVcard(vcardList)).Methods("DELETE")
+	router := server.NewRouter(vcardList)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
