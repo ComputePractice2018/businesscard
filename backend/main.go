e7b4d5124c491491fafdde41fd3ad894f764bc4f
Ilay <discherge@gmail.ru>
2018-07-09 22:54:40 +0300

    Рефакторинг в hadlers Задача #28

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-
 	"log"
 	"net/http"
 
+	"github.com/ComputePractice2018/businesscard/backend/data"
 	"github.com/ComputePractice2018/businesscard/backend/server"
 	"github.com/gorilla/mux"
 )
@@ -12,12 +12,12 @@ import (
 func main() {
 	//name := flag.String("name", "Ilay", "имя для приветствия")
 	//flag.Parse()
-
+	vcardList := data.NewVcardList()
 	router := mux.NewRouter()
-	router.HandleFunc("/api/businesscard/vcards", server.GetVcards).Methods("GET")
-	router.HandleFunc("/api/businesscard/vcards", server.AddVcard).Methods("POST")
-	router.HandleFunc("/api/businesscard/vcards/{id}", server.EditVcard).Methods("PUT")
-	router.HandleFunc("/api/businesscard/vcards/{id}", server.DeleteVcard).Methods("DELETE")
+	router.HandleFunc("/api/businesscard/vcards", server.GetVcards(vcardList)).Methods("GET")
+	router.HandleFunc("/api/businesscard/vcards", server.AddVcard(vcardList)).Methods("POST")
+	router.HandleFunc("/api/businesscard/vcards/{id}", server.EditVcard(vcardList)).Methods("PUT")
+	router.HandleFunc("/api/businesscard/vcards/{id}", server.DeleteVcard(vcardList)).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
--- backend/server/handles.go
+++ backend/server/handles.go
@@ -5,90 +5,102 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"github.com/gorilla/mux"
 	"strconv"
+
+	"github.com/gorilla/mux"
 
 	"github.com/ComputePractice2018/businesscard/backend/data"
 )
 
 //GetVcards обрабатывает запросы на получение списка контактов
-func GetVcards(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "plain/text; charset=utf-8") 
-	w.WriteHeader(http.StatusOK)
-	err := json.NewEncoder(w).Encode(data.GetVcards())
-	if err !=nil {
-		message := fmt.Sprintf("Unable to encode data: %v", err)
-		http.Error(w, message, http.StatusInternalServerError)
-		log.Println(message)
-		return
+func GetVcards(vl data.Editable) func(w http.ResponseWriter, r *http.Request) {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "plain/text; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		err := json.NewEncoder(w).Encode(vl.GetVcards())
+		if err != nil {
+			message := fmt.Sprintf("Unable to encode data: %v", err)
+			http.Error(w, message, http.StatusInternalServerError)
+			log.Println(message)
+			return
+		}
 	}
 }
 
 //AddVcard обрабатывает POST запрос
-func AddVcard(w http.ResponseWriter, r *http.Request) {
-	var vcard data.Vcard
-	err := json.NewDecoder(r.Body).Decode(&vcard)
-	if err != nil {
-		message := fmt.Sprintf("Unable to decode POST data: %v", err)
-		http.Error(w, message, http.StatusUnsupportedMediaType)
-		log.Println(message)
-		return
-	}
+func AddVcard(vl data.Editable) func(w http.ResponseWriter, r *http.Request) {
 
-	id := data.AddVcard(vcard)
-	w.Header().Add("Location", r.URL.String()+"/"+strconv.Itoa(id))
-	w.WriteHeader(http.StatusCreated)
+	return func(w http.ResponseWriter, r *http.Request) {
+		var vcard data.Vcard
+		err := json.NewDecoder(r.Body).Decode(&vcard)
+		if err != nil {
+			message := fmt.Sprintf("Unable to decode POST data: %v", err)
+			http.Error(w, message, http.StatusUnsupportedMediaType)
+			log.Println(message)
+			return
+		}
+
+		id := vl.AddVcard(vcard)
+		w.Header().Add("Location", r.URL.String()+"/"+strconv.Itoa(id))
+		w.WriteHeader(http.StatusCreated)
+	}
 }
-//EditVcard обрабатывает PUT запрос 
-func EditVcard(w http.ResponseWriter, r *http.Request) {
-	var vcard data.Vcard
-	err := json.NewDecoder(r.Body).Decode(&vcard)
-	if err != nil {
-		message := fmt.Sprintf("Unable to decode PUT data: %v", err)
-		http.Error(w, message, http.StatusUnsupportedMediaType)
-		log.Println(message)
-		return
-	}
 
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		message := fmt.Sprintf("Incorrect ID: %v", err)
-		http.Error(w, message, http.StatusUnsupportedMediaType)
-		log.Println(message)
-		return
-	}
+//EditVcard обрабатывает PUT запрос
+func EditVcard(vl data.Editable) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var vcard data.Vcard
+		err := json.NewDecoder(r.Body).Decode(&vcard)
+		if err != nil {
+			message := fmt.Sprintf("Unable to decode PUT data: %v", err)
+			http.Error(w, message, http.StatusUnsupportedMediaType)
+			log.Println(message)
+			return
+		}
 
-	err = data.EditVcard(vcard, id )
-	if err != nil {
-		message := fmt.Sprintf("Incorrect ID: %v", err)
-		http.Error(w, message, http.StatusUnsupportedMediaType)
-		log.Println(message)
-		return
-	}
-	w.Header().Add("Location", r.URL.String())
-	w.WriteHeader(http.StatusAccepted)
+		vars := mux.Vars(r)
+		id, err := strconv.Atoi(vars["id"])
+		if err != nil {
+			message := fmt.Sprintf("Incorrect ID: %v", err)
+			http.Error(w, message, http.StatusUnsupportedMediaType)
+			log.Println(message)
+			return
+		}
 
+		err = vl.EditVcard(vcard, id)
+		if err != nil {
+			message := fmt.Sprintf("Incorrect ID: %v", err)
+			http.Error(w, message, http.StatusUnsupportedMediaType)
+			log.Println(message)
+			return
+		}
+		w.Header().Add("Location", r.URL.String())
+		w.WriteHeader(http.StatusAccepted)
+
+	}
 }
 
 //DeleteVcard обрабатывает  DELETE запрос
-func DeleteVcard(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		message := fmt.Sprintf("Incorrect ID: %v", err)
-		http.Error(w, message, http.StatusUnsupportedMediaType)
-		log.Println(message)
-		return
-	}
+func DeleteVcard(vl data.Editable) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id, err := strconv.Atoi(vars["id"])
+		if err != nil {
+			message := fmt.Sprintf("Incorrect ID: %v", err)
+			http.Error(w, message, http.StatusUnsupportedMediaType)
+			log.Println(message)
+			return
+		}
 
-	err = data.RemoveVcard(id)
-	if err != nil {
-		message := fmt.Sprintf("Incorrect ID: %v", err)
-		http.Error(w, message, http.StatusUnsupportedMediaType)
-		log.Println(message)
-		return
+		err = vl.RemoveVcard(id)
+		if err != nil {
+			message := fmt.Sprintf("Incorrect ID: %v", err)
+			http.Error(w, message, http.StatusUnsupportedMediaType)
+			log.Println(message)
+			return
+		}
+		w.Header().Add("Location", r.URL.String())
+		w.WriteHeader(http.StatusNoContent)
 	}
-	w.Header().Add("Location", r.URL.String())
-	w.WriteHeader(http.StatusNoContent)
 }
