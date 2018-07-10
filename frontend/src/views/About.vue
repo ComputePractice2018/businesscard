4f6c1e70d637f863326b1ba41d66662f41fa930f
Anastasija-T <anastasija-t23@mail.ru>
2018-07-11 02:42:30 +0300

    Добавлена поддержка id и bootstrap Задача #37

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,7 +1,27 @@
 <template>
   <div class="about">
     <h1>О программе</h1>
-    <a href="https://github.com/ComputePractice2018/businesscard"
+    <div class="inf"><br><b>Приложение создано в рамках прохождения учебной практики
+     студентами ИАТЭ НИЯУ МИФИ группы ИВТ1-Б15
+      Тепляковой А.Р.,
+      Ковалевым И.В.,
+      Мироновым С.А.</b>
+      <br><br><a href="https://github.com/ComputePractice2018/businesscard"
     target="blank">Проект на GitHub</a>
+    </div>
   </div>
 </template>
+<style>
+.about {
+  height: 700px;
+}
+.inf {
+    background-color: #ffeff2 !important;
+    margin: auto;
+    font-size: 19px !important;
+    box-shadow: 0 0 5px 2px rgb(231, 194, 200);
+    width: 950px;
+    font-family: 'Segoe Script';
+    text-align: center;
+}
+</style>
